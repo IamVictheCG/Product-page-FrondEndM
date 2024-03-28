d95a060be17b7fa00f0cff7a4a35340574f81baf
Victor Okechukwu <vicoke360@gmail.com>
2024-03-28 05:05:02 +0100

    updates made

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 console.log($(".heading h3"));
 console.log("hello");
-let imageProduct_1_url = "images/image-product-1.jpg"
-let imageProduct_2_url = "images/image-product-2.jpg"
-let imageProduct_3_url = "images/image-product-3.jpg"
-let imageProduct_4_url = "images/image-product-4.jpg"
+let imageProduct_1_url = "images/image-product-1.jpg";
+let imageProduct_2_url = "images/image-product-2.jpg";
+let imageProduct_3_url = "images/image-product-3.jpg";
+let imageProduct_4_url = "images/image-product-4.jpg";
 
 let imageThumbnail_1_url = "images/image-product-1-thumbnail.jpg";
 let imageThumbnail_2_url = "images/image-product-2-thumbnail.jpg";
@@ -12,7 +12,7 @@ let imageThumbnail_4_url = "images/image-product-4-thumbnail.jpg";
 let control_plus = $(".productCount > .increase");
 let control_minus = $(".productCount > .decrease");
 // let imageThumbnails = $('.thumbnail');
-let imageThumbnails = Array(...$('.thumbnail'));
+let imageThumbnails = Array(...$(".thumbnail"));
 // console.log(imageThumbnails[2]);
 
 let mainImg = $(".mainImg");
@@ -23,52 +23,61 @@ let cartIcon = $(".cart_img");
 let addToCart = $("#addToCart");
 let deleteCartItem;
 let cartContent = $(".content");
+// let cartContentNumber = Array (...$(cartContent).children());
+// let cartContentLength = cartContentNumber.length;
+// console.log(cartContentLength);
 let empty = $(".empty");
-let price = $('.price p:nth-of-type(1)')
-let checkout = $('#checkout')
-
-let productCount = $(".productCount p")
+let price = $(".price p:nth-of-type(1)");
+let checkout = $("#checkout");
+let productCount = $(".productCount p");
 let count = 0;
 let cartCount = 0;
 let currentIndex = 0;
-let next = $(".next")
-let previous = $(".previous")
+let next = $(".next");
+let previous = $(".previous");
 let innerWidth = $(window).innerWidth();
 console.log(innerWidth);
 let desktop;
 
+const imageProducts = Array(
+    imageProduct_1_url,
+    imageProduct_2_url,
+    imageProduct_3_url,
+    imageProduct_4_url
+);
 
-const imageProducts = Array(imageProduct_1_url,imageProduct_2_url, imageProduct_3_url, imageProduct_4_url);
-
-const imageThumbnail_url = Array(imageThumbnail_1_url,imageThumbnail_2_url, imageThumbnail_3_url, imageThumbnail_4_url);
+const imageThumbnail_url = Array(
+    imageThumbnail_1_url,
+    imageThumbnail_2_url,
+    imageThumbnail_3_url,
+    imageThumbnail_4_url
+);
 
 //=========================preview========================================
 let preview = $(".preview");
 let preview_mainImg = $(".preview_mainImg");
 let preview_menu = $(".preview_menu");
 let preview_closeMenu = $(".preview_close");
-let preview_next = $(".preview_next")
-let preview_previous = $(".preview_previous")
-let preview_imageThumbnails = Array(...$('.preview_thumbnail'));
+let preview_next = $(".preview_next");
+let preview_previous = $(".preview_previous");
+let preview_imageThumbnails = Array(...$(".preview_thumbnail"));
 //=========================================================================
-
 
 console.log(imageProducts);
 console.log(imageThumbnail_url);
 console.log(mainImg.attr("src"));
 
-
-$(next).click(function (e) { 
+$(next).click(function (e) {
     e.preventDefault();
     currentIndex = (currentIndex + 1) % imageProducts.length; // Increment index cyclically
     console.log(currentIndex);
     $(mainImg).attr("src", imageProducts[currentIndex]);
     console.log($(mainImg).attr("src"));
 });
-$(previous).click(function (e) { 
-    
+$(previous).click(function (e) {
     e.preventDefault();
-    currentIndex = (currentIndex - 1 + imageProducts.length) % imageProducts.length; // Increment index cyclically
+    currentIndex =
+        (currentIndex - 1 + imageProducts.length) % imageProducts.length; // Increment index cyclically
     console.log(currentIndex);
     $(mainImg).attr("src", imageProducts[currentIndex]);
     console.log($(mainImg).attr("src"));
@@ -77,142 +86,156 @@ $(previous).click(function (e) {
 function countControl() {
     $(control_plus).click(function () {
         count++;
-        console.log(count);
-        $(productCount).text(count)
+        // console.log(count);
+        $(productCount).text(count);
     });
 
     $(control_minus).click(function () {
-        if(count === 0) {
-            return
+        if (count === 0) {
+            return;
         } else {
             count--;
-            console.log(count);
-            $(productCount).text(count)
+            // console.log(count);
+            $(productCount).text(count);
         }
     });
 }
 
 function controlNavbar() {
-    $(menu).click(function() {
-        $(".navbar-nav").addClass('showNavbar');
-        $(".navbar-nav").removeClass('hideNavbar');
-        $(".black_cover").css('display', 'block');
-    })
-    
-    $(closeMenu).click(function() {
-        $(".navbar-nav").addClass('hideNavbar');
-        $(".navbar-nav").removeClass('showNavbar');
-        $(".black_cover").css('display', 'none');
-    })
+    $(menu).click(function () {
+        $(".navbar-nav").addClass("showNavbar");
+        $(".navbar-nav").removeClass("hideNavbar");
+        $(".black_cover").css("display", "block");
+    });
+
+    $(closeMenu).click(function () {
+        $(".navbar-nav").addClass("hideNavbar");
+        $(".navbar-nav").removeClass("showNavbar");
+        $(".black_cover").css("display", "none");
+    });
 }
 
 function cartHandler() {
-    $(cartIcon).click(function() {
-        if ($(cart).css('display') == 'block') {
-            $(cart).css('display', 'none');
-            console.log($(cart).css('display'));
+    $(cartIcon).click(function () {
+        if ($(cart).css("display") == "block") {
+            $(cart).css("display", "none");
+            console.log($(cart).css("display"));
         } else {
-            $(cart).css('display', 'block');
-            console.log($(cart).css('display'));
-            if(cartCount < 1) {
-                $(cartContent).css('display', 'none');
-                $(empty).css('display', 'flex');
+            $(cart).css("display", "block");
+            console.log($(cart).css("display"));
+            if (cartCount < 1) {
+                $(cartContent).css("display", "none");
+                $(empty).css("display", "flex");
             }
         }
-    })
+    });
 }
+let cartContentLength;
+// console.log($(cartContent).children);
 
 function addToCartHandler() {
-        $(addToCart).click(function () {
-            $(cartContent).css('display', 'flex');
+    $(addToCart).click(function () {
+        cartCount++;
+        cartContentLength = cartContent.children.length; // Update cartContentLength
+        let totalPrice = $(price).text().slice(1) * count;
+        console.log(cartContentLength);
 
-            cartCount++;
-            console.log(cartCount);
-            let totalPrice = $(price).text().slice(1) * count;
-            console.log(totalPrice);
+        let newCartProduct = `
+        <div class="cartProduct">
+        <img src="${imageProducts[currentIndex]}" alt="product" />
+        <p>Fall Limited Edition Sneakers <br> $125.00 x <span class="itemQty">${count}</span>  <span class="totalPrice">$${totalPrice}</span></p>
+        <img class="delete" src="images/icon-delete.svg" alt="delete">
+        </div>`;
 
-            // Create HTML for the new cart product
-            let newCartProduct = `
-            <div class="cartProduct">
-                <img src="${imageProducts[currentIndex]}" alt="product" />
-                <p>Fall Limited Edition Sneakers <br> $125.00 x <span class="itemQty">${count}</span>  <span class="totalPrice">$${totalPrice}</span></p>
-                <img class="delete" src="images/icon-delete.svg" alt="delete">
-            </div>`;
+        // Check if both count and cartContentLength are 0
+        // if (count === 0 && cartContentLength === 0) {
+        //     $(cartContent).css("display", "none");
+        //     $(empty).css("display", "flex");
+        // } else {
+        //     $(cartContent).css("display", "flex");
+        //     $(empty).css("display", "none");
+        // }
 
-            // Find the last product in the cart
-            let lastProduct = $(cartContent).find(checkout);
-            console.log(lastProduct);
+        if (count > 0) {
+            $(".productCount p").css("color", "black");
+            $(".productCount").css("background-color", "#e6e6e6");
+        }
 
-            // Insert the new cart product before the last product
-            $(newCartProduct).insertBefore(lastProduct);
+        if (count === 0) {
+            $(".productCount p").css("color", "red");
+            $(".productCount").css("background-color", "#702d2d");
+            newCartProduct = null;
+            cartCount = 0;
+        }
 
-            if(cartCount > 0) {
-                // $(cartContent).css('display', 'none');
-                $(empty).css('display', 'none');
-            }
+        if (cartCount > 0) {
+            $(empty).css("display", "none");
+            $(cartContent).css("display", "flex");
+        }
 
+        // Find the last product in the cart
+        let lastProduct = $(cartContent).find(checkout);
+        // Insert the new cart product before the last product
+        $(newCartProduct).insertBefore(lastProduct);
+    });
 
-        })
-
-        // $(deleteCartItem).click(function () {
-
-        // })
-
-        // Event delegation to handle delete button clicks
-    $(document).on('click', '.delete', function() {
+    // Event delegation to handle delete button clicks
+    $(document).on("click", ".delete", function () {
         cartCount--;
         // Find the parent element of the delete button (which is the container for the product)
-        let product = $(this).closest('.cartProduct');
+        let product = $(this).closest(".cartProduct");
 
         // Perform actions to delete the item or hide the container
         product.remove(); // Removes the entire container from the DOM
 
-        if(cartCount < 1) {
-            $(cartContent).css('display', 'none');
-            $(empty).css('display', 'flex');
+        if (cartCount < 1) {
+            $(cartContent).css("display", "none");
+            $(empty).css("display", "flex");
         }
     });
 }
 
-$(checkout).click(function() {
-    $(cart).css('display', 'none');    
-})
+
+$(checkout).click(function () {
+    $(cart).css("display", "none");
+});
 
 //=============================================================
 //Desktop View
 
 function desktopView() {
-    $.each(imageThumbnails, function(index, element) {
-        $(element).click(function() {
+    $.each(imageThumbnails, function (index, element) {
+        $(element).click(function () {
             if ($(this)) {
-                $(this).find('.blur').css('opacity', '0.7');
-                $(imageThumbnails).not($(this)).find('.blur').css('opacity', '0');
+                $(this).find(".blur").css("opacity", "0.7");
+                $(imageThumbnails).not($(this)).find(".blur").css("opacity", "0");
                 // console.log($(this).attr('id'));
-                $(mainImg).attr('src', imageProducts[index])
+                $(mainImg).attr("src", imageProducts[index]);
                 // console.log(index);
-            } 
+            }
             // else {
             // }
-        })
+        });
         // $(element).find('img').css('border-color', '#e27500');
-    })
+    });
 
     //========preview==========
 
-    $.each(preview_imageThumbnails, function(index, element) {
-        $(element).click(function() {
+    $.each(preview_imageThumbnails, function (index, element) {
+        $(element).click(function () {
             if ($(this)) {
-                $(this).find('.preview_blur').css('opacity', '0.7');
-                $(preview_imageThumbnails).not($(this)).find('.blur').css('opacity', '0');
+                $(this).find(".preview_blur").css("opacity", "0.7");
+                $(preview_imageThumbnails)
+                    .not($(this))
+                    .find(".blur")
+                    .css("opacity", "0");
                 // console.log($(this).attr('id'));
-                $(preview_mainImg).attr('src', imageProducts[index])
+                $(preview_mainImg).attr("src", imageProducts[index]);
                 // console.log(index);
-            } 
-        })
-    })
-
+            }
+        });
+    });
 }
-
 
 //=========================preview========================================
 // let preview_mainImg = $(".preview_mainImg");
@@ -221,40 +244,38 @@ function desktopView() {
 // let preview_next = $(".preview_next")
 // let preview_previous = $(".preview_previous")
 
-$(mainImg).click(function (e) { 
+$(mainImg).click(function (e) {
     e.preventDefault();
-    $(".black_cover").css('display', 'block');
-    $(".preview").css('display', 'flex');
+    $(".black_cover").css("display", "block");
+    $(".preview").css("display", "flex");
 });
 
-// $(preview_closeMenu).mouseover(function () { 
+// $(preview_closeMenu).mouseover(function () {
 //     $(preview_closeMenu).attr('fill', 'red');
-    
+
 // });
 
-
-$(preview_closeMenu).click(function (e) { 
-e.preventDefault();
-    $(".black_cover").css('display', 'none');
-    $(".preview").css('display', 'none');
+$(preview_closeMenu).click(function (e) {
+    e.preventDefault();
+    $(".black_cover").css("display", "none");
+    $(".preview").css("display", "none");
 });
 
-$(preview_next).click(function (e) { 
+$(preview_next).click(function (e) {
     e.preventDefault();
     currentIndex = (currentIndex + 1) % imageProducts.length; // Increment index cyclically
     console.log(currentIndex);
     $(preview_mainImg).attr("src", imageProducts[currentIndex]);
     console.log($(preview_mainImg).attr("src"));
 });
-$(preview_previous).click(function (e) { 
-    
+$(preview_previous).click(function (e) {
     e.preventDefault();
-    currentIndex = (currentIndex - 1 + imageProducts.length) % imageProducts.length; // Increment index cyclically
+    currentIndex =
+        (currentIndex - 1 + imageProducts.length) % imageProducts.length; // Increment index cyclically
     console.log(currentIndex);
     $(preview_mainImg).attr("src", imageProducts[currentIndex]);
     console.log($(preview_mainImg).attr("src"));
 });
-
 
 controlNavbar();
 countControl();
